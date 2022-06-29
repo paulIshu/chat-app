b454b6b793e31c2dc075cc6f55f63d6d803cda5c
isha paul <ishapaulll30@gmail.com>
2022-06-29 21:38:59 +0530

    firebase initialization

---
+++ src/misc/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+
+
+const config = 
+
+{
+    apiKey: "AIzaSyDtnXFP3h-5XXAeRAZxbHcLNS5pp7gSqlQ",
+    authDomain: "chat-web-wpp.firebaseapp.com",
+    databaseURL: "https://chat-web-wpp-default-rtdb.firebaseio.com",
+    projectId: "chat-web-wpp",
+    storageBucket: "chat-web-wpp.appspot.com",
+    messagingSenderId: "295210810969",
+    appId: "1:295210810969:web:d4bcbe9fda1975facdbb48"
+  };
+  const app = firebase.initializeApp(config)
+  
