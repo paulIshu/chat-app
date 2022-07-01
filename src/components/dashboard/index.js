62a930bd575de30b5ce3b2710095871ceca9c6b8
isha paul <ishapaulll30@gmail.com>
2022-07-02 03:41:47 +0530

    signin on home pagefor google and facebook

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,29 @@
-import { Button, Divider, Drawer } from "rsuite";
-import { useProfile } from "../../context/profile.context";
-import EtidableInput from "../EtidableInput";
+import React from 'react';
 
+import { Drawer, Button, Divider, Alert } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
+import EtidableInput from '../EtidableInput';
+import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
 
   const onSave = async (newData )=>{
-    console.log(newData);
-  };
+      console.log(newData);
+      const userNicknameRef = database
+        .ref(`/profiles/${profile.uid}`)
+        .child('name');
+  
+      try {
+        await userNicknameRef.set(newData);
+  
+        Alert.success('Nickname has been updated', 4000);
+      } catch (err) {
+        Alert.error(err.message, 4000);
+      }
+    };
+  
   return (
     <>
       <Drawer.Header>
@@ -17,6 +32,7 @@ const Dashboard = ({ onSignOut }) => {
 
       <Drawer.Body>
         <h3> Hey, {profile.name}</h3>
+        <ProviderBlock/>
         <Divider/>
         <EtidableInput
         name ="Nickname"
